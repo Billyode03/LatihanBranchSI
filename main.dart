14f041485856742a497a521564f4d2718913d504
BillyOde03 <naubilode@gmail.com>
2024-02-26 13:13:06 +0800

    Penambahan fitur pada featureB

---
+++ main.dart
@@ -0,0 +1,10 @@
+import 'dart:io';
+
+void main() {
+  print('Penambahan fitur pada feature A');
+  print('Program Sederhana tampil nama dan umur');
+  String nama = stdin.readLineSync()!;
+  int umur = int.parse(stdin.readLineSync()!);
+
+  
+}
