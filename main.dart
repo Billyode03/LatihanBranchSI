810a0abdf7cbd52f1737dd2caecec51e2183399b
BillyOde03 <naubilode@gmail.com>
2024-02-26 13:58:22 +0800

    Menambahkan fitur untuk menginput tahun lahir

--- main.dart
+++ main.dart
@@ -1,4 +1,9 @@
-void main(){
+import 'dart:io';
+
+void main() {
   print('MEMBUAT WORKFLOW SEDERHANA');
+  print('PROGRAM HITUNG UMUR SEKARANG');
+  print('Masukkan Tahun Lahir Anda : ');
+  int tahunLahir = int.parse(stdin.readLineSync()!);
   
 }
